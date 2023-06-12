7b2f6abe4adaef98365d95ad89bf9d71b6658519
PopovVK <vladsmartvladsmart@gmail.com>
2023-06-12 13:33:40 +0300

    final

---
+++ index.js
@@ -0,0 +1,14 @@
+function generateKey(length, characters) {
+    let key = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        key += characters.charAt(randomIndex);
+    }
+    return key;
+}
+
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(10, characters);
+alert(key); 
